9368f94fff5a40be4f6a32049975554f9d534831
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-04-05 13:07:47 +0530

    App: Tailwind CSS added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 export default function Home(){
     return <>
-    <h1>Welcome to Academify</h1>
-    <p>We're coming soon...</p>
+    <h1 className="text-3xl font-bold text-center">
+    Welcome to Academify
+    </h1>
+    <p className="text-center">We're coming soon...</p>
     </>
 }
